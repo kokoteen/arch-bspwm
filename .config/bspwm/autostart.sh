e5ca47f29ea802d2bda5ef743624b28209b19d00
kokoteen <milosmilosevic994@gmail.com>
2019-10-29 02:07:20 +0100

    added kdeconnect

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -44,3 +44,4 @@ compton --config $HOME/.config/bspwm/compton.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 #run thunar &
+run kdeconnect-indicator &
