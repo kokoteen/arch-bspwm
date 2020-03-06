eaab10a43955a8859b9c887e151a2dea83ffaaa6
kokoteen <milosmilosevic994@gmail.com>
2020-03-06 09:51:51 +0100

    updated rofi position

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -10,32 +10,39 @@ function run {
 #find out your monitor name with xrandr or arandr (save and you get this line)
 autorandr -c --force
 
-#create .clock_docked if it doesnt exist /runs only once, probobly
+#create/configures .clock_docked 
 #for conky clock
-FILE=$HOME/.clock_docked
-if [ $(xrandr | grep " connected" | wc | awk '{print $1}') == "2" ] && [ ! -f "$FILE" ]; then
-	#copy & rename cloc_primary for docked monitor
-	cp $HOME/.config/bspwm/clock_primary $HOME/
-	mv $HOME/clock_primary $HOME/.clock_docked
-	#edit file
-	sed -i "s/alignment = 'top_middle'/alignment = 'top_right'/" $HOME/.clock_docked
-	sed -i "s/own_window_title = 'concky_clock_primary'/own_window_title = 'concky_clock_docked'/" $HOME/.clock_docked
-	#show clock based on a value in .monitor_position
-	if [ $(cat $HOME/.monitor_position) == "1" ]; then
-		#left position
-		sed -i "s/xinerama_head = 0/xinerama_head = 1/" $HOME/.clock_docked
-	elif [ $(cat $HOME/.monitor_position) == "2" ]; then
-		#right position
-		sed -i "s/xinerama_head = 0/xinerama_head = 2/" $HOME/.clock_docked
+if [ $(xrandr | grep -w connected | wc | awk '{print $1}') == "2" ]; then
+	#if FILE doesn't exist
+	FILE=$HOME/.clock_docked
+	if [ ! -f "$FILE" ]; then
+		#copy & rename cloc_primary for docked monitor
+		cp $HOME/.config/bspwm/clock_primary $HOME/
+		mv $HOME/clock_primary $HOME/.clock_docked
+		#edit file
+		sed -i "s/alignment = 'top_middle'/alignment = 'top_right'/" $HOME/.clock_docked
+		sed -i "s/own_window_title = 'concky_clock_primary'/own_window_title = 'concky_clock_docked'/" $HOME/.clock_docked
+	fi
+
+	#check if the position and clock differ
+	if [ $(cat .clock_docked | grep -w xinerama_head | awk -F'[, ]' '{print $3}') != $(cat $HOME/.monitor_position) ]; then
+		#show clock based on a value in .monitor_position
+		if [ $(cat $HOME/.monitor_position) == "1" ]; then
+			#left position
+			sed -i "s/xinerama_head = 0/xinerama_head = 1/" $HOME/.clock_docked
+		elif [ $(cat $HOME/.monitor_position) == "2" ]; then
+			#right position
+			sed -i "s/xinerama_head = 0/xinerama_head = 2/" $HOME/.clock_docked
+    fi
 	fi
 fi
 
-#conky clock
-if [ $(xrandr | grep " connected" | wc | awk '{print $1}') == "2" ]; then
+#run conky clock
+if [ $(xrandr | grep -w connected | wc | awk '{print $1}') == "2" ]; then
 	killall conky
 	conky -c $HOME/.config/bspwm/clock_primary &
 	conky -c $HOME/.clock_docked &
-elif [ $(xrandr | grep " connected" | wc | awk '{print $1}') == "1" ]; then
+elif [ $(xrandr | grep -w connected | wc | awk '{print $1}') == "1" ]; then
 	killall conky
 	conky -c $HOME/.config/bspwm/clock_primary &
 fi
