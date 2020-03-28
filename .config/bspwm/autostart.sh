f678bb046864cd2bff4fc99c76e598b2f3e9ec6c
kokoteen <milosmilosevic994@gmail.com>
2020-03-28 13:20:42 +0100

    changed polybar time format

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -7,6 +7,9 @@ function run {
   fi
 }
 
+#create .monitor_position if it doesn't exist
+[ ! -f "$HOME/.monitor_position" ] && echo -en "monitor_position 1\nmirror_mode 0" > $HOME/.monitor_position
+
 #find out your monitor name with xrandr or arandr (save and you get this line)
 autorandr -c --force
 
@@ -18,18 +21,18 @@ if [ $(xrandr | grep -w connected | wc | awk '{print $1}') == "2" ]; then
 	if [ ! -f "$FILE" ]; then
 		#copy & rename cloc_primary for docked monitor
 		cp $HOME/.config/bspwm/clock_primary $HOME/
-		mv $HOME/clock_primary $HOME/.clock_docked
+		mv $HOME/clock_primary $FILE
 		#edit file
-		sed -i "s/alignment = 'top_middle'/alignment = 'top_right'/" $HOME/.clock_docked
-		sed -i "s/own_window_title = 'concky_clock_primary'/own_window_title = 'concky_clock_docked'/" $HOME/.clock_docked
+		sed -i "s/alignment = 'top_middle'/alignment = 'top_right'/" $FILE
+		sed -i "s/own_window_title = 'concky_clock_primary'/own_window_title = 'concky_clock_docked'/" $FILE
 	fi
 
 	#check if the position and clock differ
-	if [ $(grep -w xinerama_head $HOME/.clock_docked | awk -F'[, ]' '{print $3}') != $(grep -w monitor_position $HOME/.monitor_position | awk '{print $2}') ]; then
+	if [ $(grep -w xinerama_head $FILE | awk -F'[, ]' '{print $3}') != $(grep -w monitor_position $HOME/.monitor_position | awk '{print $2}') ]; then
   		#left position
-		[ $(grep -w monitor_position $HOME/.monitor_position | awk '{print $2}') -eq "1" ] && sed -i "s/xinerama_head = 0/xinerama_head = 1/" $HOME/.clock_docked
+		[ $(grep -w monitor_position $HOME/.monitor_position | awk '{print $2}') -eq "1" ] && sed -i "s/xinerama_head = 0/xinerama_head = 1/" $FILE
 		# right position
-		[ $(grep -w monitor_position $HOME/.monitor_position | awk '{print $2}') -eq "2" ] && sed -i "s/xinerama_head = 0/xinerama_head = 2/" $HOME/.clock_docked
+		[ $(grep -w monitor_position $HOME/.monitor_position | awk '{print $2}') -eq "2" ] && sed -i "s/xinerama_head = 0/xinerama_head = 2/" $FILE
 	fi
 fi
 
