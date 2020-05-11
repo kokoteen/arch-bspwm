5eb1990d232fba2425f3b89d96451c624b5380a4
kokoteen <milosmilosevic994@gmail.com>
2020-05-11 02:14:35 +0200

    kdeconnect fix polybar

--- .config/polybar/scripts/polybar-kdeconnect.sh
+++ .config/polybar/scripts/polybar-kdeconnect.sh
@@ -4,8 +4,8 @@
 LOCATION=0
 YOFFSET=0
 XOFFSET=0
-WIDTH=12
-WIDTH_WIDE=24
+WIDTH=200
+WIDTH_WIDE=420
 THEME=themes/kde_connect
 
 # Color Settings of Icon shown in Polybar
@@ -19,14 +19,14 @@ COLOR_BATTERY_50='#666'         # Battery >= 50
 COLOR_BATTERY_LOW='#f00'        # Battery <  50
 
 # Icons shown in Polybar
-ICON_SMARTPHONE=''
-ICON_TABLET=''
-SEPERATOR='|'
+ICON_SMARTPHONE=''
+ICON_TABLET=''
+ICON_DEKTOP=''
+SEPERATOR=' '
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 show_devices (){
-    IFS=$','
     devices=""
     for device in $(qdbus --literal org.kde.kdeconnect /modules/kdeconnect org.kde.kdeconnect.daemon.devices); do
         deviceid=$(echo "$device" | awk -F'["|"]' '{print $2}')
@@ -38,18 +38,19 @@ show_devices (){
         then
             battery="$(qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$deviceid" org.kde.kdeconnect.device.battery.charge)"
             icon=$(get_icon "$battery" "$devicetype")
-            devices+="%{A1:$DIR/polybar-kdeconnect.sh -n '$devicename' -i $deviceid -b $battery -m:}$icon%{A}$SEPERATOR"
+            devices+="%{A1:$DIR/polybar-kdeconnect.sh -n '$devicename' -i $deviceid -b $battery -m:}$icon%{A}$SEPERATOR" # show_menu
         elif [ "$isreach" = "false" ] && [ "$istrust" = "true" ]
         then
-            devices+="$(get_icon -1 "$devicetype")$SEPERATOR"
+            devices+="$(get_icon -1 "$devicetype")$SEPERATOR"   # disconnected
         else
             haspairing="$(qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$deviceid" org.kde.kdeconnect.device.hasPairingRequests)"
             if [ "$haspairing" = "true" ]
             then
-                show_pmenu2 "$devicename" "$deviceid"
+                show_pmenu2 "$devicename" "$deviceid"   # pair request
             fi
-            icon=$(get_icon -2 "$devicetype")
-            devices+="%{A1:$DIR/polybar-kdeconnect.sh -n $devicename -i $deviceid -p:}$icon%{A}$SEPERATOR"
+            icon=$(get_icon -2 "$devicetype")       
+            devices+="%{A1:$DIR/polybar-kdeconnect.sh -n $devicename -i $deviceid -p:}$icon%{A}$SEPERATOR"  # connect
+
 
         fi
     done
@@ -57,33 +58,33 @@ show_devices (){
 }
 
 show_menu () {
-    menu="$(rofi -sep "|" -dmenu -i -p "$DEV_NAME" -location $LOCATION -yoffset $YOFFSET -xoffset $XOFFSET -theme $THEME -width $WIDTH -hide-scrollbar -line-padding 4 -padding 20 -lines 5 <<< "Battery: $DEV_BATTERY%|Ping|Find Device|Send File|Browse Files|Unpair")"
+    menu="$(rofi -sep "|" -dmenu -i -p "$DEV_NAME" -theme $THEME -width $WIDTH -lines 6 <<< "Battery: $DEV_BATTERY%|Ping|Find Device|Send File|Browse Files|Unpair")"
                 case "$menu" in
-                    *Ping) qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/ping" org.kde.kdeconnect.device.ping.sendPing ;;
-                    *'Find Device') qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/findmyphone" org.kde.kdeconnect.device.findmyphone.ring ;;
-                    *'Send File') qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/share" org.kde.kdeconnect.device.share.shareUrl "file://$(zenity --file-selection)" ;;
-                    *'Browse Files')
-                        if "$(qdbus --literal org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/sftp" org.kde.kdeconnect.device.sftp.isMounted)" == "false"; then
-                            qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/sftp" org.kde.kdeconnect.device.sftp.mount
-                        fi
-                        qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/sftp" org.kde.kdeconnect.device.sftp.startBrowsing
-                        ;;
-                    *'Unpair' ) qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID" org.kde.kdeconnect.device.unpair
+                    'Ping')         qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/ping" org.kde.kdeconnect.device.ping.sendPing ;;
+                    'Find Device')  qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/findmyphone" org.kde.kdeconnect.device.findmyphone.ring ;;
+                    'Send File')    qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/share" org.kde.kdeconnect.device.share.shareUrl "file://$(zenity --file-selection)" ;;
+                    'Browse Files')
+                                    if "$(qdbus --literal org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/sftp" org.kde.kdeconnect.device.sftp.isMounted)" == "false"; then
+                                        qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/sftp" org.kde.kdeconnect.device.sftp.mount
+                                    fi
+                                    qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID/sftp" org.kde.kdeconnect.device.sftp.startBrowsing
+                                    ;;
+                    'Unpair')       qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID" org.kde.kdeconnect.device.unpair
                 esac
 }
 
 show_pmenu () {
-    menu="$(rofi -sep "|" -dmenu -i -p "$DEV_NAME" -location $LOCATION -yoffset $YOFFSET -xoffset $XOFFSET -theme $THEME -width $WIDTH -hide-scrollbar -line-padding 1 -padding 20 -lines 1<<<"Pair Device")"
+    menu="$(rofi -sep "|" -dmenu -i -p "$DEV_NAME" -theme $THEME -width $WIDTH -lines 1 <<< "Pair Device")"
                 case "$menu" in
-                    *'Pair Device') qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID" org.kde.kdeconnect.device.requestPair
+                    'Pair Device')  qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$DEV_ID" org.kde.kdeconnect.device.requestPair
                 esac
 }
 
 show_pmenu2 () {
-    menu="$(rofi -sep "|" -dmenu -i -p "$1 has sent a pairing request" -location $LOCATION -yoffset $YOFFSET -xoffset $XOFFSET -theme $THEME -width $WIDTH_WIDE -hide-scrollbar -line-padding 4 -padding 20 -lines 2 <<< "Accept|Reject")"
+    menu="$(rofi -sep "|" -dmenu -i -p "$1 has sent a pairing request" -theme $THEME -width $WIDTH_WIDE -lines 2 <<< "Accept|Reject")"
                 case "$menu" in
-                    *'Accept') qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$2" org.kde.kdeconnect.device.acceptPairing ;;
-                    *) qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$2" org.kde.kdeconnect.device.rejectPairing
+                    'Accept')       qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$2" org.kde.kdeconnect.device.acceptPairing ;;
+                    *)              qdbus org.kde.kdeconnect "/modules/kdeconnect/devices/$2" org.kde.kdeconnect.device.rejectPairing
                 esac
 
 }
@@ -91,12 +92,15 @@ get_icon () {
     if [ "$2" = "tablet" ]
     then
         icon=$ICON_TABLET
+    elif [ "$2" = "desktop" ]
+    then
+        icon=$ICON_DEKTOP
     else
         icon=$ICON_SMARTPHONE
     fi
     case $1 in
-    "-1")     ICON="%{F$COLOR_DISCONNECTED}$icon%{F-}" ;;
-    "-2")     ICON="%{F$COLOR_NEWDEVICE}$icon%{F-}" ;;
+    "-1")   ICON="%{F$COLOR_DISCONNECTED}$icon%{F-}" ;;
+    "-2")   ICON="%{F$COLOR_NEWDEVICE}$icon%{F-}" ;;
     5*)     ICON="%{F$COLOR_BATTERY_50}$icon%{F-}" ;;
     6*)     ICON="%{F$COLOR_BATTERY_60}$icon%{F-}" ;;
     7*)     ICON="%{F$COLOR_BATTERY_70}$icon%{F-}" ;;
