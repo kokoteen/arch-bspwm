3626b650a7e437e3bcd18aa35383439557e919db
kokoteen <milosmilosevic994@gmail.com>
2020-11-26 16:26:16 +0100

    removed Thunar uca.xml

--- .bin/yayfzf.sh
+++ .bin/yayfzf.sh
@@ -3,4 +3,4 @@
 yay -Sl | \
 awk '{print $2($4=="" ? "" : " *")}'| \
 fzf -e -m --preview 'cat <(yay -Si {1}) <(yay -Fl {1} | awk "{print \$2}")' --layout=reverse | \
-xargs -ro -n1 yay -S
+xargs -ro -n1 yay -Sy
